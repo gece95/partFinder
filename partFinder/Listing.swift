55e08bda9c7d2ad3162ef631b4df6c392855b859
Zoe Hazan <zoehazan@gmail.com>
2025-03-04 14:22:58 -0800

    created Listing class for our dummy listngs for sprint 1 until we have a working database.

--- partFinder/Listing.swift
+++ partFinder/Listing.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+class Listing {
+    var title = ""
+    var partType = "" // Battery, brakes, etc.
+    var make = ""
+    var model = ""
+    var year = ""
+    var trim = ""
+    var price = 0.0
+    var description = ""
+    var city = ""
+    var phoneNumber = ""
+    
+    init(title: String, partType: String, make: String, model: String, year: String, trim: String, price: Double, description: String, city: String, phoneNumber: String) {
+        self.title = title
+        self.partType = partType // Battery, brakes, etc.
+        self.make = make
+        self.model = model
+        self.year = year
+        self.trim = trim
+        self.price = price
+        self.description = description
+        self.city = city
+        self.phoneNumber = phoneNumber
+    }
+}
