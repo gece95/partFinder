70aba375afed3038c3f814030771d292f78fe0a9
Emily Marrufo <emilymarrufo@hotmail.com>
2025-03-25 15:22:48 -0700

    changed the entire UI

---
+++ Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  schoolProject
+//
+//  Created by Emily marrufo on 3/24/25.
+//
+
+import Foundation
+
+struct Category: Identifiable {
+    let id = UUID()
+    let icon: String
+    let label: String
+}
